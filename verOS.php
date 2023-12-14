6e2da3ad1b4282f745e61a76eb891571229c24f7
Luís Felipe Monteiro do Carmo <luivszzz@gmail.com>
2023-12-14 20:47:28 -0300

    Finalizado.

--- verOS.php
+++ verOS.php
@@ -52,42 +52,143 @@ $estadoOS = $row["orderEstado"];
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link href="./css/a4.css" rel="stylesheet">
         <link rel="shortcut icon" href="./img/favicon-16x16.png" type="image/x-icon">
+        <script defer type="text/javascript">
+            
+
+    function printpage() {
+        var printButton = document.getElementById("print");
+        document.getElementById('print').classList.add("d-none");
+        document.getElementById('back').classList.add("d-none");
+        window.print()
+        document.getElementById('print').classList.remove("d-none");
+        document.getElementById('back').classList.remove("d-none");
+
+    }
+</script>
+<style>
+    table {
+width:500px;
+table-layout:fixed;
+}
+
+
+td {
+word-wrap:break-word;
+}
+</style>
     </head>
     <body>
+    <div class="position-fixed bottom-0 end-1 mb-3 ms-3 bd-mode-toggle">
+    <a type="submit" id="back" href="./index.php" class="btn btn-secondary w-100 d-flex align-items-center">Voltar</a>
+  </div>
+  <div class="position-fixed bottom-0 end-0 mb-3 ms-3 bd-mode-toggle">
+    <a type="submit" class="btn btn-primary w-100 d-flex align-items-center" id="print" onclick="printpage()">Imprimir</a>
+  </div>
         <div class="book">
             <div class="page">
                 <div class="subpage text-center">
                     
                     <h1 class="h3 mb-3 fw-bold"><img class="mb-2" src="./img/icon.png" alt="" width="30" height="30"> João Manutenção - Ordem de Serviço</h1>
-                    <p>Nome do cliente: <?= $nomeCliente;?></p>
                     <table class="table text-start table-bordered align-middle">
                         <thead class="table-active">
                             <tr>
-                                <th scope="col" colspan="4"><h1 class="h3 fw-bold">Cliente</h1></th>
+                                <th scope="col" colspan="7"><h1 class="h3 fw-bold">Cliente</h1></th>
                             </tr>
                         </thead>
                         <tbody>
                         <tr>
                             <th scope="row">Nome:</th>
-                            <td colspan="3">Mark</td>
-                            </tr>
-                            <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
+                            <td colspan="3"><?= $nomeCliente ?></td>
+                            <th scope="row">CPF:</th>
+                            <td colspan="2"><?= $cpfCliente ?></td>
                         </tr>
+                        <tr>
+                            <th scope="row">Endereço:</th>
+                            <td colspan="6"><?= $endereco ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Cidade</th>
+                            <td><?= $infoAdic ?></td>
+                            <th scope="row">CEP</th>
+                            <td><?= $infoAdic ?></td>
+                            <th scope="row">Bairro</th>
+                            <td colspan="2"><?= $infoAdic ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row" colspan="7">Informações adicionais:</th>
+                        </tr>
+                        <tr>
+                            <td colspan="7"><?= $infoAdic ?></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="table text-start table-bordered align-middle">
+                        <thead class="table-active">
                             <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
+                                <th scope="col" colspan="3"><h1 class="h3 fw-bold">Produto</h1></th>
                             </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">Modelo:</th>
+                            <td colspan="2"><?= $prdtModelo ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Detalhes:</th>
+                            <td colspan="2"><?= $prdtDetalhes ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row" colspan="3">Reclamação:</th>
+                        </tr>
+                        <tr>
+                            <td colspan="3"><?= $prdtReclam ?></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="table text-start table-bordered align-middle">
+                        <thead class="table-active">
+                            <tr>
+                                <th scope="col" colspan="6"><h1 class="h3 fw-bold">Serviço</h1></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row" colspan="6">Diagnóstico e serviço a ser executado:</th>
+                        </tr>
+                        <tr>
+                            <td colspan="6"><?= $servDiag ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row" colspan="6">Garantia e outras observações:</th>
+                        </tr>
+                        <tr>
+                            <td colspan="6"><?= $servGarant ?></td>
+                        </tr>
+                        <tr>
+                            <th scope="row" colspan="2">Valor do serviço</th>
+                            <th scope="row" colspan="2">Valor de peças/produtos</th>
+                            <th scope="row" colspan="2">Valor total</th>
+                        </tr>
+                            <td colspan="2">R$ <?= $valorServ ?></td>
+                            <td colspan="2">R$ <?= $valorPeca ?></td>
+                            <td colspan="2">R$ <?= $valorServ+$valorPeca ?></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="table text-start table-bordered align-middle">
+                        <tbody>
+                        <tr>
+                            <th scope="row"><br><br><br><br><br></th>
+                            <th scope="row"><br></th>
+                        </tr>
+                        <tr>
+                            <td scope="row">Assinatura do prestador</th>
+                            <td scope="row">Assinatura do cliente</th>
+                        </tr>
+
                         </tbody>
                     </table>
                 </div>
-            </div>
-            <div class="page">
-                <div class="subpage">Page 2/2</div>    
             </div>
         </div>
     </body>
